0c11b4af0992f29e1b49dff1c304b444439a4d45
Brian Cervantes <briancervantes@Brians-Air.lan>
2023-10-27 23:25:25 -0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import NucampLogo from './app/assets/img/logo.png'
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Placeholder Text
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='nucamp logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
     </div>
   );
 }

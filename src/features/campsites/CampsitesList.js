035dd0dac186a862ef25179f7a5902e48c0f7f19
Brian Cervantes <briancervantes@Brians-Air-48.lan>
2023-10-30 18:03:36 -0700

    Adding functionality with useParams and react router dom

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,7 +2,7 @@ import CampsiteCard from './CampsiteCard';
 import { Col, Row } from 'reactstrap';
 import { selectAllCampsites } from './campsitesSlice';
 
-const CampsiteLists = ({ setCampsiteId }) => {
+const CampsiteLists = () => {
     const campsites = selectAllCampsites();
 
     return(
@@ -10,10 +10,9 @@ const CampsiteLists = ({ setCampsiteId }) => {
             {campsites.map((campsite) => {
                 return (
                     <Col 
-                    md='5' c
-                    lassName='m-4' 
+                    md='5' 
+                    className='m-4' 
                     key={campsite.id}
-                    onClick={() => setCampsiteId(campsite.id)}
                     >
                         <CampsiteCard campsite={campsite} />
                     </Col>
